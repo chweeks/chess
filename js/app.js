d0b85ce67c66ffd6975edd3b93a8405d20946917
chweeks <chrisharryweeks@gmail.com>
2016-01-11 17:31:48 +0000

    added board and cells

--- js/app.js
+++ js/app.js
@@ -1,20 +1,104 @@
 'use strict';
 
 var Header = React.createClass({
-  render: function(){
-    return(
-      <h1>
-        Unlease Chess
-      </h1>
+  render: function() {
+    return (
+      <header>
+        <h1>Unlease Chess</h1>
+      </header>
     );
   }
 });
 
+var ChessBoard = React.createClass({
+  render: function() {
+    return (
+      <div className="board">
+        <Cell />
+      </div>
+    );
+  }
+});
+
+var Cell = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+      </div>
+    )
+  }
+})
+
 var UnleaseChess = React.createClass({
   render: function() {
     return (
-      <div class="container">
+      <div className="container">
         <Header />
+        <ChessBoard />
       </div>
     );
   },
